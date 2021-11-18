a849f54c0610a96c345ab64ee6aced17b7140363
Karina Lyapina <ivettra@yandex.ru>
2021-11-18 20:27:48 +0500

    feat: создать схему пользователя

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const isEmail = require('validator/lib/isEmail');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => isEmail(email),
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
