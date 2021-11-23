be02eb18850bdec96ead178866eba7f908e2be28
Karina Lyapina <ivettra@yandex.ru>
2021-11-23 22:24:35 +0500

    commit

--- routes/storage.js
+++ routes/storage.js
@@ -1,6 +1,6 @@
 const router = require('express').Router(); // создали роутер
 const { celebrate, Joi } = require('celebrate');
-const { createUser, login } = require('../controllers/users');
+const { createUser, login, logout } = require('../controllers/users');
 
 router.post(
   '/signup',
