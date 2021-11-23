a0238f7b276f13396b4fa592613b450d0e9e6d9b
Karina Lyapina <ivettra@yandex.ru>
2021-11-23 22:41:57 +0500

    fix: метод signout

--- controllers/users.js
+++ controllers/users.js
@@ -127,6 +127,11 @@ module.exports.login = (req, res, next) => {
 
 module.exports.logout = (req, res) => {
   res
-    .clearCookie('jwt')
-    .end();
+    .cookie('jwt', '', {
+      maxAge: -1,
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+    })
+    .send({ message: 'Ваша cookie больше не куки' });
 };
